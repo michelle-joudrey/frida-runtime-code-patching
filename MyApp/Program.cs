a88fa8f615b14381e4176e983af44c03edad74a1
Ricky Joudrey <rjoudrey@users.noreply.github.com>
2016-04-07 00:19:19 -0400

    Write the hooker

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -10,8 +10,23 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            var stringPrinter = new MyLibrary.MyStringPrinter();
-            stringPrinter.PrintString();
+            // MyLibrary.dll won't get loaded into the runtime until we use MyLibraryClass,
+            // so for the hooker work, we use MyLibraryClass once, and then wait for the 
+            // hooker to do it's work.
+            MyLibrary.MyLibraryClass myLibraryClassInstance = new MyLibrary.MyLibraryClass();
+            bool before = myLibraryClassInstance.MyLibraryMethod();
+            Console.WriteLine(before);
+            Console.ReadLine();
+            if (myLibraryClassInstance.MyLibraryMethod())
+            {
+                Console.WriteLine("Success");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
