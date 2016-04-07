baca8cf619c0e661f51d8f2f72c8fc80fc2e2260
Ricky Joudrey <rjoudrey@users.noreply.github.com>
2016-04-07 01:09:59 -0400

    Refactor hook script

--- hookMyApp.py
+++ hookMyApp.py
@@ -2,37 +2,34 @@ import frida
 import os
 
 targetProcessName = "MyApp.exe"
-session = frida.attach(targetProcessName)
+try:
+    session = frida.attach(targetProcessName)
+except Exception as ex:
+    print ex
+    os.system('pause')
+    sys.exit(-1)
 script = session.create_script("""
-var targetProcessName = 'MyApp.exe';
 var module = Process.enumerateModulesSync()
     .filter(function(x) {
-        return x.name == targetProcessName;
+        return x.name == '%s';
     })[0];
+var patched = false;
 Process.enumerateRanges('rw-', {
     onMatch: function(range) {
         var matches = Memory.scanSync(range.base, range.size, '90 33 d2 89 55 ?? 90');
         if (matches.length == 0) {
             return;
         }
-        if (matches.length > 1) {
-            console.log('Something has gone terribly wrong');
-            return;
-        }
         var match = matches[0];
-        console.log('Found pattern at ' + match.address);
-        Memory.writeByteArray(match.address, [0x31, 0xD2, 0x42]);
-        matches = Memory.scanSync(range.base, range.size, '31 d2 42 89 55 ?? 90');
-        if (matches.length == 0) {
-            console.log('Failed to write memory');
-            return;
-        }
-        console.log('Succesfully wrote memory');
+        Memory.writeByteArray(match.address, [ 0x31, 0xD2, 0x42 ]);
+        console.log('Patched code at ' + match.address);
+        patched = true;
     },
-    onComplete: function() {
-        console.log('complete');
-    }
+    onComplete: function() { }
 });
-""")
+if (!patched) {
+   console.log('Failed to patch code');
+}
+""" % targetProcessName)
 script.load()
 os.system('pause')
